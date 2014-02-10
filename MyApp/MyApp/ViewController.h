c92d576a40dd042887d35c2a783c1df2512e1375
Dal Rupnik <legoless@gmail.com>
2014-02-10 20:00:05 +0100

    Project initialization.
    Prepared project, installed pods.

---
+++ MyApp/MyApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Dal Rupnik on 10/02/14.
+//  Copyright (c) 2014 arvystate.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
